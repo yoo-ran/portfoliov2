9984b0c5487ddeabbe2ff8497eca5af940bbb70b
YooranKim <yuranm@naver.com>
2026-03-27 15:01:27 -0700

    fix exported images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  turbopack: {
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
